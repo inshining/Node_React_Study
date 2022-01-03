71eb847cac4c6dd7c6e52986c77ff17db6aa7cc3
INYEOB <inshining97@gmail.com>
2022-01-03 21:49:34 +0900

    Notice : 비밀 설정 정보 관리

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kimInyeob:kiy0430@boilerplate.tigx3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-    useNewUrlParser: true, useUnifidedTopology: true, useCreateIndex: true, useFindAndModify: true
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('MongoDB Connected')).catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 안녕하세요!')
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
 })
 
 app.listen(port, () => {
