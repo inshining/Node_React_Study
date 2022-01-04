5701c68494ef84f814e681ba04577f1e02d4fba4
INYEOB <inshining97@gmail.com>
2022-01-04 17:49:48 +0900

    [ADD] : 로그 아웃 구현

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const { User } = require("./models/User");
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
+const { auth } = require('./middleware/auth');
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
@@ -60,6 +61,32 @@ app.post('/api/users/login', (req, res) => {
     });
   });
 });
+
+app.get('/api/users/auth', auth , (req, res) => {
+
+  // 여기까지 미들웨어를 통과해 왔다는 애기는 Authentication 이 true라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role == 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({_id: req.user._id},
+  { token: ""},
+  (err, user) => {
+    if (err) return res.json({ success: false, err});
+    return res.status(200).send({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- models/User.js
+++ models/User.js
@@ -80,6 +80,22 @@ userSchema.methods.generateToken = function(cb) {
     })
 }
 
+userSchema.statics.findByToken = function(token, cb) {
+    var user = this;
+
+    // user._id + '' = token
+    // 토큰을 decode 한다.
+
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        // 유저 아이디 이요앻서 유저를 찾은 다음에 
+        // 클라이언트에서 가져오 token과 DB 보관된 토큰이 일치하는 지 확인
+
+        user.findOne({"_id": decoded, "token": token }, function(err, user){
+            if (err) return cb(err);
+            cb(null, user);
+        })
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
