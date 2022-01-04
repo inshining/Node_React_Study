86b5002d3b39062b72337ef93e7be36f2513f7ed
INYEOB <inshining97@gmail.com>
2022-01-04 15:28:10 +0900

    [update] : token 생성

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -26,6 +27,9 @@ const userSchema = mongoose.Schema({
     },
     image: String,
     token: {
+        type: String
+    },
+    tokenExp: {
         type: Number
     }
 
@@ -48,10 +52,33 @@ userSchema.pre('save', function( next ) {
             })
 
         })
+    } else {
+        next()
     }
- 
 });
     
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+userSchema.methods.generateToken = function(cb) {
+
+    var user = this;
+
+    // jsonwebtoken를 이용해서 token 생성
+
+    var token = jwt.sign(user._id.toHexString(),  'secretToken')
+
+    user.token = token;
+    user.save(function(err, user) {
+        if(err) return cb(err);
+        cb(null, user);
+    })
+}
 
 
 const User = mongoose.model('User', userSchema)
