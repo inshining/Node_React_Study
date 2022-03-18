c8700bc7dad15e6c21a5c8ba4f0484000e60d62c
inshining <inshining@yonsei.ac.kr>
2022-03-18 12:56:18 +0900

    API로 비트코인 정보 받아오기

--- movie-web/src/App.js
+++ movie-web/src/App.js
@@ -2,31 +2,27 @@ import styles from "./App.module.css"
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault()
-      if (toDo === "") {
-        return;
-      }    
-      setToDos((currentArray) => [toDo, ...currentArray] )
-    setToDo("");
-  };
+  const [loading, setLoading] = useState(true);
+  const [conis, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((response) => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    });
+  },[]);
   return (
     <div>
-      <h1>My To Dos {toDos.length}</h1>
-      <form onSubmit={onSubmit}>
-      <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do.." />
-      <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-      {toDos.map((item, index) => (
-      <li key={index}>{item}</li>
-      ))}
-      </ul>
-      </div>
+      <h1>The Coins!{loading ? "" : (conis.length)}</h1>
+      {loading ? 
+        (<strong>Loading...</strong>)
+      : (<select>
+        {conis.map((item) => 
+          <option>{item.name} ({item.symbol}) : {item.quotes.USD.price} USD</option>
+        )}
+      </select>)}
+    </div>
   );
 }
 
