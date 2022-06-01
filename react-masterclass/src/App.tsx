c88e6a771ecc5f71befb8e62fb5ecedffe0b6b90
inshining <inshining97@gmail.com>
2022-06-01 14:35:03 +0900

    6.4 palceholder

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -1,99 +1,64 @@
-import { createGlobalStyle } from "styled-components";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import styled from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;1,300&family=Noto+Sans+KR:wght@300;500&display=swap');
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, menu, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-main, menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-  margin: 0;
-  padding: 0;
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, main, menu, nav, section {
-  display: block;
-}
-/* HTML5 hidden-attribute fix for newer browsers */
-*[hidden] {
-    display: none;
-}
-body {
-  line-height: 1;
-  font-family: 'Lato', sans-serif;
-  font-family: 'Noto Sans KR', sans-serif;
-}
-a {
-  text-decoration:none;
-};
-menu, ol, ul {
-  list-style: none;
-}
-blockquote, q {
-  quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-  content: '';
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
-* {
-  box-sizing: border-box;
-}
-body {
-  font-family: 'Source Sans Pro', sans-serif;
-  background-color:${(props) => props.theme.bgColor};
-  color:${(props) => props.theme.textColor}
-}
-a {
-  text-decoration:none;
-  color: inherit;
-}
+const Boards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  width: 100%;
 `
+
+const Board = styled.div`
+  padding: 20px 10px;
+  padding-top:30px;
+  background-color: ${props => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`
+
+const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+  background-color: ${props => props.theme.cardColor};
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  max-width: 480px;
+  width: 100%;
+  margin: 0 auto;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const toDos = ["a", "b", "c", "d", "e", "f"];
+
 
 function App() {
   const onDragEnd = () => {}
   return (
     <>
       <DragDropContext onDragEnd={onDragEnd}>
-        <div>
+        <Wrapper>
+          <Boards>
           <Droppable droppableId="one">
             {(magic) => (
-            <ul ref={magic.innerRef} {...magic.droppableProps}>
-              <Draggable draggableId="first" index={0}>
+            <Board ref={magic.innerRef} {...magic.droppableProps}>
+              {toDos.map((toDo, index) => (
+              <Draggable draggableId={toDo} index={index}>
                 {(magic) => (
-                <li ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
-                  One
-                </li>
+                <Card ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
+                  {toDo}
+                </Card>
                 )}
               </Draggable>
-              <Draggable draggableId="second" index={1}>
-                {(magic) => (
-                <li ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
-                  Two
-                </li>)}
-              </Draggable>
-            </ul>)}
+              ))}
+              {magic.placeholder}
+            </Board>)}
           </Droppable>
-        </div>  
+         </Boards>
+        </Wrapper>  
       </DragDropContext> 
     </>
   )
