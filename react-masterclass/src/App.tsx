47c0cd37407ee1b060bb17abb79e3dc69d71845f
inshining <inshining97@gmail.com>
2022-06-01 14:58:01 +0900

    6.6 Reordering Two

--- react-masterclass/src/App.tsx
+++ react-masterclass/src/App.tsx
@@ -38,8 +38,15 @@ const Wrapper = styled.div`
 
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState)
-  const onDragEnd = ({destination, source}:DropResult) => {
-    console.log(arg);
+  const onDragEnd = ({draggableId,destination, source}:DropResult) => {
+    if (!destination) return;
+    setToDos((oldToDos) => {
+      const copyToDos = [...oldToDos];
+      copyToDos.splice(source.index, 1);
+      copyToDos.splice(destination?.index, 0, draggableId)
+      return copyToDos
+
+    })
     
   }
   return (
@@ -51,7 +58,7 @@ function App() {
             {(magic) => (
             <Board ref={magic.innerRef} {...magic.droppableProps}>
               {toDos.map((toDo, index) => (
-              <Draggable key={index} draggableId={toDo} index={index}>
+              <Draggable key={toDo} draggableId={toDo} index={index}>
                 {(magic) => (
                 <Card ref={magic.innerRef} {...magic.draggableProps} {...magic.dragHandleProps}>
                   {toDo}
